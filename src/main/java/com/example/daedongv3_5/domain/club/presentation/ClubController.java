60e40457cc270c97c161edf85867c090e113f1de
troy96475 <taesupark0504@gmail.com>
2025-03-11 17:15:15 +0900

    feat :: club controller

--- src/main/java/com/example/daedongv3_5/domain/club/presentation/ClubController.java
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/ClubController.java
@@ -1,2 +1,20 @@
-package com.example.daedongv3_5.domain.club.presentation;public class ClubController {
+package com.example.daedongv3_5.domain.club.presentation;
+
+import com.example.daedongv3_5.domain.club.application.CreateClubService;
+import com.example.daedongv3_5.domain.club.presentation.dto.request.ClubRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/club")
+@RequiredArgsConstructor
+public class ClubController {
+    private final CreateClubService createClubService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createClub(@RequestBody ClubRequest request) {
+        createClubService.createClub(request);
+    }
 }
