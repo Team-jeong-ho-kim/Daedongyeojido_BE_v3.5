7c252958d64d5c600d1d678d5891bac5abd1f8e8
troy96475 <taesupark0504@gmail.com>
2025-03-11 20:26:36 +0900

    refactor :: createdAt 추가

--- src/main/java/com/example/daedongv3_5/global/entity/BaseIdEntity.java
+++ src/main/java/com/example/daedongv3_5/global/entity/BaseIdEntity.java
@@ -2,7 +2,11 @@ package com.example.daedongv3_5.global.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
@@ -11,4 +15,8 @@ public abstract class BaseIdEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
