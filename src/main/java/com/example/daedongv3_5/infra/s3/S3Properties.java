de13be9250f1c237c241666cdd121a84b76a99dc
민수아 <Minsua0503@gmail.com>
2025-03-11 20:25:13 +0900

    (#1) feat :: S3Properties 추가

---
+++ src/main/java/com/example/daedongv3_5/infra/s3/S3Properties.java
@@ -0,0 +1,13 @@
+package com.example.daedongv3_5.infra.s3;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "cloud.aws.s3")
+public record S3Properties(
+    String bucket,
+    String accessKey,
+    String secretKey,
+    String profileImages,
+    String clubImages
+) {
+}
