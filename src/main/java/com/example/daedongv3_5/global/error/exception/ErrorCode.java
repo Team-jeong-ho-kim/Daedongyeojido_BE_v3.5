b0b02c5586a2f974e52613e6b1e375701b13cdaf
troy96475 <taesupark0504@gmail.com>
2025-03-12 08:40:52 +0900

    feat :: 동아리 관련예외

--- src/main/java/com/example/daedongv3_5/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/daedongv3_5/global/error/exception/ErrorCode.java
@@ -20,27 +20,15 @@ public enum ErrorCode {
     INVALID_USER(401, "유효하지 않는 사용자입니다."),
     USER_ALREADY_TAKEN(401, "이미 유저가 있습니다"),
 
+    //club
+    CLUB_NOT_FOUND(404, "존재하지 않는 동아리 입니다."),
+
     // general
     BAD_REQUEST(400, "프론트 탓이 확실하다.."),
     INTERNAL_SERVER_ERROR(500, "서버 탓일 수도 있고.."),
 
     // key
     INVALID_KEY(401, "잘못된 key입니다."),
-
-    //comment
-    COMMENT_NOT_FOUND(404, "Cannot Found Comment."),
-    CANNOT_DELETE_COMMENT(403, "Cannot Delete Comment."),
-    CANNOT_MODIFY_COMMENT(403, "Cannot Modify Comment."),
-
-    //feed
-    FEED_NOT_FOUND(404, "Feed Not Found."),
-    CANNOT_DELETE_FEED(403, "Cannot Delete Feed."),
-    CANNOT_MODIFY_FEED(403, "Cannot Modify Feed."),
-    CANNOT_CREATE_FEED(403, "Cannot Create Feed"),
-
-    //like
-    LIKE_EXIST(409, "Like Exist."),
-    CANNOT_DELETE_LIKE(409, "Cannot Delete Like."),
 
     //image
     FAIL_FILE(500, "파일 업로드 실패");
