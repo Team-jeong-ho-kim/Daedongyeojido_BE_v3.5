90198c176269e40dc58c91c2ffb50518d0ca8de8
민수아 <Minsua0503@gmail.com>
2025-03-11 20:24:56 +0900

    (#1) chore :: @ConfigurationPropertiesScan 추가

--- src/main/java/com/example/daedongv3_5/DaedongApplication.java
+++ src/main/java/com/example/daedongv3_5/DaedongApplication.java
@@ -1,12 +1,13 @@
 package com.example.daedongv3_5;
 
-import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class DaedongApplication {
 
     public static void main(String[] args) {
