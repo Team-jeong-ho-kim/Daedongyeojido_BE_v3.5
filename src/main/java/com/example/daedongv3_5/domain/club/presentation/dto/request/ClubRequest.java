62cc3687a51edc3c8aaa486f3eb1e899f23f382a
troy96475 <taesupark0504@gmail.com>
2025-03-11 17:14:52 +0900

    feat :: request

--- src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/request/ClubRequest.java
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/request/ClubRequest.java
@@ -1,2 +1,22 @@
-package com.example.daedongv3_5.domain.club.presentation.dto.request;public class ClubRequest {
+package com.example.daedongv3_5.domain.club.presentation.dto.request;
+
+import com.example.daedongv3_5.domain.club.domain.enums.Major;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ClubRequest {
+
+    @NotNull
+    private String clubName;
+
+    private String introduction;
+
+    private String clubUser;
+
+    @Enumerated(EnumType.STRING)
+    private Major major;
+
 }
