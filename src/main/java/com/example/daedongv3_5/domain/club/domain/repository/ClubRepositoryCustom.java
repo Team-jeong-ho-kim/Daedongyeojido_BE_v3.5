d2e9c5df6f07afceac9bbe8165eaa797e0254f86
troy96475 <taesupark0504@gmail.com>
2025-03-11 15:22:44 +0900

    feat :: clubrepository 구현체

---
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.example.daedongv3_5.domain.club.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubRepositoryCustom {
+}
---
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepositoryCustomImpl.java
@@ -0,0 +1,7 @@
+package com.example.daedongv3_5.domain.club.domain.repository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ClubRepositoryCustomImpl implements ClubRepositoryCustom {
+}
