1d807b3d91df3bea7401655067d7cfc221be3584
troy96475 <taesupark0504@gmail.com>
2025-03-11 15:22:29 +0900

    feat :: club repository

--- src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
@@ -1,2 +1,9 @@
-package com.example.daedongv3_5.domain.club.domain.repository;public interface ClubRepository {
+package com.example.daedongv3_5.domain.club.domain.repository;
+
+import com.example.daedongv3_5.domain.club.domain.Club;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubRepository extends CrudRepository<Club, Long>, ClubRepositoryCustom{
 }
