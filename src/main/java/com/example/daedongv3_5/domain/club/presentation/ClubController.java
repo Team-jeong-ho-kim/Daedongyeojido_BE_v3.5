72d551c22803450e1c0a367d3736efe3bd332ea9
troy96475 <taesupark0504@gmail.com>
2025-03-12 08:48:16 +0900

    feat :: 전체 조회 api

--- src/main/java/com/example/daedongv3_5/domain/club/presentation/ClubController.java
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/ClubController.java
@@ -1,20 +1,34 @@
 package com.example.daedongv3_5.domain.club.presentation;
 
 import com.example.daedongv3_5.domain.club.application.CreateClubService;
+import com.example.daedongv3_5.domain.club.application.DeleteClubService;
+import com.example.daedongv3_5.domain.club.application.QueryAllClubService;
+import com.example.daedongv3_5.domain.club.application.UpdateClubService;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.ClubRequest;
+import com.example.daedongv3_5.domain.club.presentation.dto.request.UpdateClubRequest;
+import com.example.daedongv3_5.domain.club.presentation.dto.response.ClubListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/club")
 @RequiredArgsConstructor
 public class ClubController {
     private final CreateClubService createClubService;
+    private final QueryAllClubService queryAllClubService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createClub(@RequestBody ClubRequest request) {
         createClubService.createClub(request);
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ClubListResponse> queryAllClub() {
+        return queryAllClubService.queryAllClub();
     }
 }
