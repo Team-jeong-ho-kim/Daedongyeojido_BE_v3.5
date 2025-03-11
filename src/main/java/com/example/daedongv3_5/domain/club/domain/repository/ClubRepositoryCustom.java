c738484c6a3a2f445b8b1127a6b47db043b3ca82
troy96475 <taesupark0504@gmail.com>
2025-03-12 08:47:35 +0900

    feat :: 전체조회 쿼리

--- src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepositoryCustom.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepositoryCustom.java
@@ -1,7 +1,12 @@
 package com.example.daedongv3_5.domain.club.domain.repository;
 
+import com.example.daedongv3_5.domain.club.presentation.dto.response.ClubListResponse;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClubRepositoryCustom {
+
+    List<ClubListResponse> findAllClub();
 }
--- src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepositoryCustomImpl.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepositoryCustomImpl.java
@@ -1,7 +1,25 @@
 package com.example.daedongv3_5.domain.club.domain.repository;
 
+import com.example.daedongv3_5.domain.club.domain.QClub;
+import com.example.daedongv3_5.domain.club.presentation.dto.response.ClubListResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class ClubRepositoryCustomImpl implements ClubRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QClub qClub = QClub.club;
+
+    @Override
+    public List<ClubListResponse> findAllClub() {
+        return jpaQueryFactory.select(Projections.constructor(ClubListResponse.class, qClub.clubName, qClub.introduction, qClub.clubMember, qClub.major, qClub.createdAt))
+                .from(qClub)
+                .orderBy(qClub.createdAt.desc())
+                .fetch();
+    }
+
 }
