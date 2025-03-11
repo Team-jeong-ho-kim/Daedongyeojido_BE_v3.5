71f32d2f1c4641bd427da5c44b566dffd2d306e1
troy96475 <taesupark0504@gmail.com>
2025-03-11 11:25:01 +0900

    chore :: Auditing 활성화

--- src/main/java/com/example/daedongv3_5/DaedongApplication.java
+++ src/main/java/com/example/daedongv3_5/DaedongApplication.java
@@ -1,9 +1,12 @@
 package com.example.daedongv3_5;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DaedongApplication {
 
     public static void main(String[] args) {
