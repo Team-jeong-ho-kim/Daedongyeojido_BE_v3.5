6f322162d75cd3e6927a5c4c210753a4672cf8fe
troy96475 <taesupark0504@gmail.com>
2025-03-11 17:14:12 +0900

    feat :: 동아리 생성

--- src/main/java/com/example/daedongv3_5/domain/club/application/CreateClubService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/CreateClubService.java
@@ -1,2 +1,24 @@
-package com.example.daedongv3_5.domain.club.application;public class CreateClubService {
+package com.example.daedongv3_5.domain.club.application;
+
+import com.example.daedongv3_5.domain.club.domain.Club;
+import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
+import com.example.daedongv3_5.domain.club.presentation.dto.request.ClubRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CreateClubService {
+    private final ClubRepository clubRepository;
+
+    @Transactional
+    public void createClub(ClubRequest request) {
+        clubRepository.save(Club.builder()
+                        .clubName(request.getClubName())
+                        .introduction(request.getIntroduction())
+                        .clubUser(request.getClubUser())
+                        .major(request.getMajor())
+                .build());
+    }
 }
