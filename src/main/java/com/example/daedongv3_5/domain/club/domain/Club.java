1d1747d141dd158cb525f8a93d2d972f99c45e7a
troy96475 <taesupark0504@gmail.com>
2025-03-11 14:29:15 +0900

    feat :: club 도메인

--- src/main/java/com/example/daedongv3_5/domain/club/domain/Club.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/Club.java
@@ -1,2 +1,26 @@
-package com.example.daedongv3_5.domain.club.domain;public class Club {
+package com.example.daedongv3_5.domain.club.domain;
+
+import com.example.daedongv3_5.domain.club.domain.enums.Major;
+import com.example.daedongv3_5.global.entity.BaseIdEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Club extends BaseIdEntity {
+
+    private String clubName;
+
+    private String introduction;
+
+    private String clubUser;
+
+    private Major major;
+
 }
