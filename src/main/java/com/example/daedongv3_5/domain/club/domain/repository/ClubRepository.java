c47c7bddf9195f343f6c7fc72c1a2ca8cff77143
troy96475 <taesupark0504@gmail.com>
2025-03-12 08:50:22 +0900

    feat :: 동아리 삭제 쿼리

--- src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
@@ -4,6 +4,11 @@ import com.example.daedongv3_5.domain.club.domain.Club;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClubRepository extends CrudRepository<Club, Long>, ClubRepositoryCustom{
+    Optional<Club> findByClubName(String clubName);
+
+    void deleteByClubName(String clubName);
 }
