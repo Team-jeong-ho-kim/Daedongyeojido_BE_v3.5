a02f1eb8b9ced184d86546672b0ecf34c1a0b533
troy96475 <taesupark0504@gmail.com>
2025-03-11 14:29:27 +0900

    feat :: major enum 클래스

--- src/main/java/com/example/daedongv3_5/domain/club/domain/enums/Major.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/enums/Major.java
@@ -1,4 +1,15 @@
-package com.example.daedongv3_5.domain.club.domain;
+package com.example.daedongv3_5.domain.club.domain.enums;
 
 public enum Major {
+    BACKEND,
+    FRONTEND,
+    ANDROID,
+    IOS,
+    DESIGN,
+    FLUTTER,
+    EMBEDDED,
+    AI,
+    DEVOPS,
+    SECURITY
+
 }
