180903359ccd58603c7c581f84cd7d160db1e3df
troy96475 <taesupark0504@gmail.com>
2025-03-11 17:13:57 +0900

    feat :: baseIdEntity

--- src/main/java/com/example/daedongv3_5/global/entity/BaseIdEntity.java
+++ src/main/java/com/example/daedongv3_5/global/entity/BaseIdEntity.java
@@ -1,2 +1,14 @@
-package com.example.daedongv3_5.global.entity;public class BaseIdEntity {
+package com.example.daedongv3_5.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseIdEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
