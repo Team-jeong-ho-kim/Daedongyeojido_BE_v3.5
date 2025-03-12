a237fc8c47ad4005bfade7fb930a93847ce5ef00
troy96475 <taesupark0504@gmail.com>
2025-03-12 09:30:10 +0900

    feat :: 동아리 정보 조회

---
+++ src/main/java/com/example/daedongv3_5/domain/club/application/QueryClubInfoService.java
@@ -0,0 +1,21 @@
+package com.example.daedongv3_5.domain.club.application;
+
+import com.example.daedongv3_5.domain.club.application.facade.ClubFacade;
+import com.example.daedongv3_5.domain.club.domain.Club;
+import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
+import com.example.daedongv3_5.domain.club.presentation.dto.response.ClubResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueryClubInfoService {
+    private final ClubRepository clubRepository;
+    private final ClubFacade clubFacade;
+
+    public ClubResponse queryClubInfo(String clubName) {
+        Club club = clubFacade.clubFacade(clubName);
+
+        return clubRepository.findClubInfoByClubName(clubName);
+    }
+}
