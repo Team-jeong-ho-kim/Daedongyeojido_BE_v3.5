51fd172dfe8bb46aab5130b92106a2d36b0878b9
troy96475 <taesupark0504@gmail.com>
2025-03-12 09:29:33 +0900

    refactor :: 생성자 추가

--- src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/response/ClubListResponse.java
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/response/ClubListResponse.java
@@ -1,11 +1,13 @@
 package com.example.daedongv3_5.domain.club.presentation.dto.response;
 
 import com.example.daedongv3_5.domain.club.domain.enums.Major;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
 public class ClubListResponse {
     private String clubName;
     private String introduction;
