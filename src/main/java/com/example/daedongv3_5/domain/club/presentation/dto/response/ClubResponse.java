c0ba374660f9d92694a296c83c6f6627df1f689f
troy96475 <taesupark0504@gmail.com>
2025-03-12 09:30:17 +0900

    feat :: 정보 조회 dto

---
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/response/ClubResponse.java
@@ -0,0 +1,14 @@
+package com.example.daedongv3_5.domain.club.presentation.dto.response;
+
+import com.example.daedongv3_5.domain.club.domain.enums.Major;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ClubResponse {
+    private String clubName;
+    private String introduction;
+    private String clubMember;
+    private Major major;
+}
