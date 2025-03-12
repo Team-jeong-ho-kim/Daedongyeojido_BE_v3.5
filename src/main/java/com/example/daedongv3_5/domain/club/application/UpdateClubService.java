55bee55015ad5c617c3b1b46ba64f5f619ebc8d8
troy96475 <taesupark0504@gmail.com>
2025-03-12 09:09:15 +0900

    refactor :: clubFacade 적용

--- src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubService.java
@@ -1,8 +1,8 @@
 package com.example.daedongv3_5.domain.club.application;
 
+import com.example.daedongv3_5.domain.club.application.facade.ClubFacade;
 import com.example.daedongv3_5.domain.club.domain.Club;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
-import com.example.daedongv3_5.domain.club.exception.ClubNotFoundException;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.UpdateClubRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,13 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UpdateClubService {
     private final ClubRepository clubRepository;
+    private final ClubFacade clubFacade;
 
     @Transactional
     public void updateClub(String clubName, UpdateClubRequest request) {
-        Club club = clubRepository.findByClubName(clubName)
-                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+        Club club = clubFacade.clubFacade(clubName);
 
-        club.update(request.getClubName(), request.getIntroduction(), request.getClubMember(), request.getMajor());
+        club.update(
+                request.getClubName(),
+                request.getIntroduction(),
+                request.getClubMember(),
+                request.getMajor()
+        );
     }
 
 }
