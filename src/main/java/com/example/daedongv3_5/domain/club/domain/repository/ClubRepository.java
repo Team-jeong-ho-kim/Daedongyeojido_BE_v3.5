650aa5b616f19a4a8013f1439a15d73f43692614
troy96475 <taesupark0504@gmail.com>
2025-03-13 22:54:09 +0900

    refactor :: 원래 다 분리 해야하는데 귀찮아서 그냥 한 번에 커밋할게여

--- src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
@@ -1,14 +1,21 @@
 package com.example.daedongv3_5.domain.club.domain.repository;
 
 import com.example.daedongv3_5.domain.club.domain.Club;
+import com.example.daedongv3_5.domain.club.presentation.dto.response.ClubResponse;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long>, ClubRepositoryCustom{
-    Optional<Club> findById(Long clubId);
 
-    void deleteByClubName(String clubName);
+    Optional<Club> findClubById(Long id);
+
+    void deleteById(Long id);
+
+    @EntityGraph(attributePaths = {"majors"}) // majors 필드를 즉시 로딩
+    List<Club> findAll();
 }
