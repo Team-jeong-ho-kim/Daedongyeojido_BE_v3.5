dfbdccc6e4428c50557e46d09688717ad5740bf0
troy96475 <taesupark0504@gmail.com>
2025-03-13 22:54:47 +0900

    refactor :: 수정하는거

--- src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
@@ -2,11 +2,14 @@ package com.example.daedongv3_5.domain.club.application;
 
 import com.example.daedongv3_5.domain.club.application.facade.ClubFacade;
 import com.example.daedongv3_5.domain.club.domain.Club;
+import com.example.daedongv3_5.domain.club.domain.Major;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.UpdateClubRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +18,14 @@ public class UpdateClubInfoService {
     private final ClubFacade clubFacade;
 
     @Transactional
-    public void updateClub(Long clubId, UpdateClubRequest request) {
-        Club club = clubFacade.clubFacade(clubId);
+    public void updateClub(Long id, UpdateClubRequest request) {
+        Club club = clubFacade.clubFacade(id);
 
         club.update(
                 request.getClubName(),
                 request.getIntroduction(),
                 request.getClubMember(),
-                request.getMajor()
+                request.getMajors().stream().map(majorType -> new Major(majorType)).collect(Collectors.toList())
         );
     }
 
--- src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/request/UpdateClubRequest.java
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/request/UpdateClubRequest.java
@@ -1,12 +1,20 @@
 package com.example.daedongv3_5.domain.club.presentation.dto.request;
 
-import com.example.daedongv3_5.domain.club.domain.enums.Major;
+import com.example.daedongv3_5.domain.club.domain.enums.MajorType;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class UpdateClubRequest {
+
+    @NotNull
     private String clubName;
+
     private String introduction;
+
     private String clubMember;
-    private Major major;
+
+    private List<MajorType> majors;
 }
