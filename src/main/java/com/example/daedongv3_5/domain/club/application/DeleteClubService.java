4af58ecc3c7c4ec8cad4faa5646fdda9b67299e0
troy96475 <taesupark0504@gmail.com>
2025-03-13 16:49:40 +0900

    refactor :: 삭제 예외처리 추가

--- src/main/java/com/example/daedongv3_5/domain/club/application/DeleteClubService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/DeleteClubService.java
@@ -1,5 +1,7 @@
 package com.example.daedongv3_5.domain.club.application;
 
+import com.example.daedongv3_5.domain.club.application.facade.ClubFacade;
+import com.example.daedongv3_5.domain.club.domain.Club;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DeleteClubService {
     private final ClubRepository clubRepository;
+    private final ClubFacade clubFacade;
 
     @Transactional
-    public void deleteClub(String clubName) {
-        clubRepository.deleteByClubName(clubName);
+    public void deleteClub(Long clubId) {
+        Club club = clubFacade.clubFacade(clubId);
+        clubRepository.deleteById(clubId);
     }
 }
