ab0ed509b277a7440a07c3251bfa53398f030288
troy96475 <taesupark0504@gmail.com>
2025-03-13 20:24:07 +0900

    feat :: 겁나 많이 바꿈

--- src/main/java/com/example/daedongv3_5/domain/club/domain/Club.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/Club.java
@@ -1,15 +1,15 @@
 package com.example.daedongv3_5.domain.club.domain;
 
-import com.example.daedongv3_5.domain.club.domain.enums.Major;
 import com.example.daedongv3_5.global.entity.BaseIdEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -26,14 +26,15 @@ public class Club extends BaseIdEntity {
 
     private String clubMember;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Major major;
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private List<Major> majors = new ArrayList<>();
 
-    public void update(String clubName, String introduction, String clubMember, Major major) {
-        this.clubName = clubMember;
+
+    public void update(String clubName, String introduction, String clubMember, List<Major> majors) {
+        this.clubName = clubName;
         this.introduction = introduction;
         this.clubMember = clubMember;
-        this.major = major;
+        this.majors = majors;
     }
 }
