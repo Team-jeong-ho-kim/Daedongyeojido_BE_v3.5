56fe6e2739f867d6f1ebfeecf326f43981c615c9
troy96475 <taesupark0504@gmail.com>
2025-03-13 20:22:41 +0900

    feat :: major 클래스로 분리 했어요

--- src/main/java/com/example/daedongv3_5/domain/club/domain/Major.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/Major.java
@@ -1,2 +1,24 @@
-package com.example.daedongv3_5.domain.club.domain;public class Major {
+package com.example.daedongv3_5.domain.club.domain;
+
+import com.example.daedongv3_5.domain.club.domain.enums.MajorType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Major {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MajorType majorType;
+
+    public Major(MajorType majorType) {
+        this.majorType = majorType;
+    }
 }
