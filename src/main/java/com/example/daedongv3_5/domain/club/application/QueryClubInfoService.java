41b77147764030f45d8ba3f4eee4792cf52449d9
troy96475 <taesupark0504@gmail.com>
2025-03-13 23:02:03 +0900

    refactor :: 조회하는거 야미

--- src/main/java/com/example/daedongv3_5/domain/club/application/QueryClubInfoService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/QueryClubInfoService.java
@@ -6,6 +6,10 @@ import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
 import com.example.daedongv3_5.domain.club.presentation.dto.response.ClubResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +17,13 @@ public class QueryClubInfoService {
     private final ClubRepository clubRepository;
     private final ClubFacade clubFacade;
 
-    public ClubResponse queryClubInfo(Long clubId) {
-        Club club = clubFacade.clubFacade(clubId);
+    @Transactional(readOnly = true)
+    public List<ClubResponse> queryClubInfo(Long id) {
+        Club club = clubFacade.clubFacade(id);
 
-        return clubRepository.findClubInfoByClubId(clubId);
+        return clubRepository.findClubById(id)
+                .stream()
+                .map(club1 -> new ClubResponse(club))
+                .collect(Collectors.toList());
     }
 }
