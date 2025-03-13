c00fb340c5c36512ae0bf37a0f9df0b10e5b4019
troy96475 <taesupark0504@gmail.com>
2025-03-13 22:52:08 +0900

    refactor :: 동아리 생성

--- src/main/java/com/example/daedongv3_5/domain/club/application/CreateClubService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/CreateClubService.java
@@ -1,11 +1,14 @@
 package com.example.daedongv3_5.domain.club.application;
 
 import com.example.daedongv3_5.domain.club.domain.Club;
+import com.example.daedongv3_5.domain.club.domain.Major;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.ClubRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class CreateClubService {
                         .clubName(request.getClubName())
                         .introduction(request.getIntroduction())
                         .clubMember(request.getClubMember())
-                        .major(request.getMajor())
+                        .majors(request.getMajors().stream().map(majorType -> new Major(majorType)).collect(Collectors.toList()))
                 .build());
     }
 }
