17d40e336c62e8f08d20e709d71b2ac23d6c2d10
troy96475 <taesupark0504@gmail.com>
2025-03-14 21:44:15 +0900

    refactor :: 유효성 검사 추가

--- src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/request/ClubRequest.java
+++ src/main/java/com/example/daedongv3_5/domain/club/presentation/dto/request/ClubRequest.java
@@ -1,7 +1,9 @@
 package com.example.daedongv3_5.domain.club.presentation.dto.request;
 
 import com.example.daedongv3_5.domain.club.domain.enums.MajorType;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,15 +15,18 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class ClubRequest {
 
-    @NotNull
+    @NotNull(message = "동아리 이름이 비어있습니다.")
     private String clubName;
 
+    @Size(max = 300, message = "소개를 300자 이하로 작성해주세요.")
     private String introduction;
 
+    @Size(max = 20, message = "한 줄 소개를 20자 이하로 작성해주세요.")
     private String oneLiner;
 
     private String clubMember;
 
+    @NotEmpty(message = "한 가지 이상의 전공을 선택해야 합니다.")
     private List<MajorType> majors;
 
 }
