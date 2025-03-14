3f741e4a668545bcf83a7d2f05dcb8b4f09908ac
troy96475 <taesupark0504@gmail.com>
2025-03-14 13:24:29 +0900

    refactor :: time관련 필드 삭제

--- src/main/java/com/example/daedongv3_5/global/entity/BaseIdEntity.java
+++ src/main/java/com/example/daedongv3_5/global/entity/BaseIdEntity.java
@@ -3,7 +3,6 @@ package com.example.daedongv3_5.global.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -16,7 +15,5 @@ public abstract class BaseIdEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+
 }
