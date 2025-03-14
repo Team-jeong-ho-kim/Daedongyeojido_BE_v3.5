2512bb10b28f59702126bb9bc7bf527f2d881b43
민수아 <Minsua0503@gmail.com>
2025-03-14 10:56:29 +0900

    refactor :: 불필요한 properties 삭제

--- src/main/java/com/example/daedongv3_5/infra/s3/S3Properties.java
+++ src/main/java/com/example/daedongv3_5/infra/s3/S3Properties.java
@@ -5,8 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "cloud.aws.s3")
 public record S3Properties(
     String bucket,
-    String accessKey,
-    String secretKey,
     String profileImages,
     String clubImages
 ) {
