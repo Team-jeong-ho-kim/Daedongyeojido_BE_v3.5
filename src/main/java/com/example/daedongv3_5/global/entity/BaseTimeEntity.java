ec1cc3e731bb2e7c4c02dc09fe499959990e1d16
troy96475 <taesupark0504@gmail.com>
2025-03-14 13:24:43 +0900

    feat :: time 관련 baseEntity

---
+++ src/main/java/com/example/daedongv3_5/global/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.example.daedongv3_5.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AutoCloseable.class)
+public abstract class BaseTimeEntity extends BaseIdEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
