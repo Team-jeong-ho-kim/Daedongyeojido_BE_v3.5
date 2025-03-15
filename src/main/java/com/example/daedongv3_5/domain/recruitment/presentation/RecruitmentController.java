d9bc27a469e480916e7123d3a4a3f501841a0a4c
민수아 <Minsua0503@gmail.com>
2025-03-16 00:26:46 +0900

    feat :: UpdateRecruitmentService 추가

--- src/main/java/com/example/daedongv3_5/domain/recruitment/presentation/RecruitmentController.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/presentation/RecruitmentController.java
@@ -1,6 +1,7 @@
 package com.example.daedongv3_5.domain.recruitment.presentation;
 
 import com.example.daedongv3_5.domain.recruitment.application.CreateRecruitmentService;
+import com.example.daedongv3_5.domain.recruitment.application.UpdateRecruitmentService;
 import com.example.daedongv3_5.domain.recruitment.presentation.dto.request.RecruitmentRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +14,18 @@ import org.springframework.web.bind.annotation.*;
 public class RecruitmentController {
 
     private final CreateRecruitmentService createRecruitmentService;
+    private final UpdateRecruitmentService updateRecruitmentService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createRecruitment(@RequestBody @Valid RecruitmentRequest request) {
         createRecruitmentService.createRecruitment(request);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateRecruitment(@PathVariable Long id, @RequestBody @Valid RecruitmentRequest request) {
+        updateRecruitmentService.updateRecruitment(id, request);
     }
 
 }
