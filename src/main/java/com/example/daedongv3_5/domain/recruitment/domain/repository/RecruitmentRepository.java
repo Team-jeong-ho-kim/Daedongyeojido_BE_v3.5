be6342d6706e186d44e9c7db54e8ccac42a9c409
민수아 <Minsua0503@gmail.com>
2025-03-16 00:15:25 +0900

    feat :: RecruitmentRepository 추가

---
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
@@ -0,0 +1,7 @@
+package com.example.daedongv3_5.domain.recruitment.domain.repository;
+
+import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+}
