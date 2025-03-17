3289c148e5907904569be414ff20f1b5020d4521
troy96475 <taesupark0504@gmail.com>
2025-03-17 12:31:16 +0900

    refactor :: major 객체로 변환하지 않고 enum으로 받기

--- src/main/java/com/example/daedongv3_5/domain/club/application/CreateClubService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/CreateClubService.java
@@ -1,14 +1,11 @@
 package com.example.daedongv3_5.domain.club.application;
 
 import com.example.daedongv3_5.domain.club.domain.Club;
-import com.example.daedongv3_5.domain.club.domain.Major;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.ClubRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +19,7 @@ public class CreateClubService {
                         .introduction(request.getIntroduction())
                         .oneLiner(request.getOneLiner())
                         .clubMember(request.getClubMember())
-                        .majors(request.getMajors().stream().map(majorType -> new Major(majorType)).collect(Collectors.toList()))
+                        .majors(request.getMajors())
                 .build());
     }
 }
