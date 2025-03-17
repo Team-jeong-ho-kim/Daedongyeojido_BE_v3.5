f1fe37ea06dd2ab610e44f7eaf4a9645467fa933
troy96475 <taesupark0504@gmail.com>
2025-03-17 12:33:10 +0900

    refactor :: repository 안써도 됨

--- src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepositoryCustom.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepositoryCustom.java
@@ -1,9 +1,9 @@
-package com.example.daedongv3_5.domain.recruitment.domain.repository;
-
-import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
-
-public interface RecruitmentRepositoryCustom {
-
-    Recruitment findByRecruitmentId(Long id);
-
-}
+//package com.example.daedongv3_5.domain.recruitment.domain.repository;
+//
+//import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
+//
+//public interface RecruitmentRepositoryCustom {
+//
+//    Recruitment findByRecruitmentId(Long id);
+//
+//}
--- src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepositoryCustomImpl.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepositoryCustomImpl.java
@@ -1,25 +1,25 @@
-package com.example.daedongv3_5.domain.recruitment.domain.repository;
-
-import com.example.daedongv3_5.domain.club.domain.QMajor;
-import com.example.daedongv3_5.domain.recruitment.domain.QRecruitment;
-import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCustom {
-
-    private final JPAQueryFactory queryFactory;
-    private final QRecruitment recruitment = QRecruitment.recruitment;
-    private final QMajor major = QMajor.major;
-
-    @Override
-    public Recruitment findByRecruitmentId(Long id) {
-        return queryFactory
-            .selectFrom(recruitment)
-            .leftJoin(recruitment.majors, major).fetchJoin()
-            .where(recruitment.id.eq(id))
-            .fetchOne();
-    }
-
-}
+//package com.example.daedongv3_5.domain.recruitment.domain.repository;
+//
+//import com.example.daedongv3_5.domain.club.domain.QMajor;
+//import com.example.daedongv3_5.domain.recruitment.domain.QRecruitment;
+//import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import lombok.RequiredArgsConstructor;
+//
+//@RequiredArgsConstructor
+//public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCustom {
+//
+//    private final JPAQueryFactory queryFactory;
+//    private final QRecruitment recruitment = QRecruitment.recruitment;
+//    private final QMajor major = QMajor.major;
+//
+//    @Override
+//    public Recruitment findByRecruitmentId(Long id) {
+//        return queryFactory
+//            .selectFrom(recruitment)
+//            .leftJoin(recruitment.majors, major).fetchJoin()
+//            .where(recruitment.id.eq(id))
+//            .fetchOne();
+//    }
+//
+//}
