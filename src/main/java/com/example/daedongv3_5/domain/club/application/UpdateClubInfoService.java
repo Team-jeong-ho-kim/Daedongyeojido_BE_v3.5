decb5fd9cee025e9c60bc8b369f0e892b7d06945
troy96475 <taesupark0504@gmail.com>
2025-03-17 12:35:04 +0900

    refactor :: 수정하는 메서드 변경후 적용

--- src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
@@ -2,14 +2,11 @@ package com.example.daedongv3_5.domain.club.application;
 
 import com.example.daedongv3_5.domain.club.application.facade.ClubFacade;
 import com.example.daedongv3_5.domain.club.domain.Club;
-import com.example.daedongv3_5.domain.club.domain.Major;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.UpdateClubRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,12 +18,7 @@ public class UpdateClubInfoService {
     public void updateClub(Long id, UpdateClubRequest request) {
         Club club = clubFacade.clubFacade(id);
 
-        club.update(
-                request.getClubName(),
-                request.getIntroduction(),
-                request.getOneLiner(),
-                request.getMajors().stream().map(majorType -> new Major(majorType)).collect(Collectors.toList())
-        );
+        club.update(request);
     }
 
 }
