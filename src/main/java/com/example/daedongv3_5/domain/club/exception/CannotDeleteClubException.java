74d10ac928278c403498164e823a982b781b605f
troy96475 <taesupark0504@gmail.com>
2025-03-18 19:24:24 +0900

    feat :: 삭제 권한 예외처리

---
+++ src/main/java/com/example/daedongv3_5/domain/club/exception/CannotDeleteClubException.java
@@ -0,0 +1,12 @@
+package com.example.daedongv3_5.domain.club.exception;
+
+import com.example.daedongv3_5.global.error.exception.DaedongException;
+import com.example.daedongv3_5.global.error.exception.ErrorCode;
+
+public class CannotDeleteClubException extends DaedongException {
+    public static final CannotDeleteClubException EXCEPTION = new CannotDeleteClubException();
+
+    public CannotDeleteClubException() {
+        super(ErrorCode.CANNOT_DELETE_CLUB);
+    }
+}
