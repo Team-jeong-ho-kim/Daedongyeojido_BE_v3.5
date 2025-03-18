41d7fa6efc2b735cfcd72b423f7da5319bd137e3
troy96475 <taesupark0504@gmail.com>
2025-03-18 21:31:38 +0900

    feat :: 동아리 및 지원서 수정시 해당 유저만 가능하도록

--- src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
@@ -3,7 +3,7 @@ package com.example.daedongv3_5.domain.club.application;
 import com.example.daedongv3_5.domain.auth.service.facade.UserFacade;
 import com.example.daedongv3_5.domain.club.domain.Club;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
-import com.example.daedongv3_5.domain.club.exception.CannotModifyClubInfoException;
+import com.example.daedongv3_5.domain.club.exception.CannotUpdateClubInfoException;
 import com.example.daedongv3_5.domain.club.exception.ClubNotFoundException;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.UpdateClubRequest;
 import com.example.daedongv3_5.domain.student.domain.StudentEntity;
@@ -25,7 +25,7 @@ public class UpdateClubInfoService {
         StudentEntity student = userFacade.currentUser();
 
         if (!club.getCreatedBy().equals(student.getAccountId())) {
-            throw new CannotModifyClubInfoException();
+            throw new CannotUpdateClubInfoException();
         }
 
         club.update(request);
