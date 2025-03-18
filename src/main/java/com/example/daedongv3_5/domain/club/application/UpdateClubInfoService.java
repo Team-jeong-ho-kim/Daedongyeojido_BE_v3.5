9e49fca1dea566cc8b275c247aa5ed77f6c25c19
troy96475 <taesupark0504@gmail.com>
2025-03-18 19:24:36 +0900

    feat :: 수정 권한 예외처리

--- src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
+++ src/main/java/com/example/daedongv3_5/domain/club/application/UpdateClubInfoService.java
@@ -1,9 +1,12 @@
 package com.example.daedongv3_5.domain.club.application;
 
+import com.example.daedongv3_5.domain.auth.service.facade.UserFacade;
 import com.example.daedongv3_5.domain.club.domain.Club;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
+import com.example.daedongv3_5.domain.club.exception.CannotModifyClubInfoException;
 import com.example.daedongv3_5.domain.club.exception.ClubNotFoundException;
 import com.example.daedongv3_5.domain.club.presentation.dto.request.UpdateClubRequest;
+import com.example.daedongv3_5.domain.student.domain.StudentEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UpdateClubInfoService {
     private final ClubRepository clubRepository;
+    private final UserFacade userFacade;
 
     @Transactional
     public void updateClub(UpdateClubRequest request) {
         Club club = clubRepository.findById(request.getId())
                 .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+
+        StudentEntity student = userFacade.currentUser();
+
+        if (!club.getCreatedBy().equals(student.getAccountId())) {
+            throw new CannotModifyClubInfoException();
+        }
 
         club.update(request);
     }
