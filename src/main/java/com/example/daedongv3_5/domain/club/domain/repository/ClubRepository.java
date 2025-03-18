e119dd787132de8de4c7b7b706f60fae2baad49b
troy96475 <taesupark0504@gmail.com>
2025-03-18 15:35:10 +0900

    refactor :: 동아리 이름으로 조회 함수

--- src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/club/domain/repository/ClubRepository.java
@@ -3,6 +3,9 @@ package com.example.daedongv3_5.domain.club.domain.repository;
 import com.example.daedongv3_5.domain.club.domain.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubRepository extends JpaRepository<Club, Long> {
+    Optional<Club> findRecruitmentByClubName(String clubName);
 
 }
