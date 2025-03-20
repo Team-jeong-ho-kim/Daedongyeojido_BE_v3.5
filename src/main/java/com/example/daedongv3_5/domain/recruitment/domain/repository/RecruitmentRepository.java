9e193d5fa953fe26c9ae883c4846501e219986ff
troy96475 <taesupark0504@gmail.com>
2025-03-20 22:26:58 +0900

    feat :: 조회 쿼리

--- src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
@@ -4,7 +4,10 @@ import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
     List<Recruitment> findRecruitmentByClub_ClubName(String clubName);
+
+    Optional<Recruitment> findRecruitmentByAccountId(String accountId);
 }
