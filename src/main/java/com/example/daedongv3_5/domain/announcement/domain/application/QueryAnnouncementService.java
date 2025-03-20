fe43834f5921ad11e1d75f53d32b65335f9b0a78
troy96475 <taesupark0504@gmail.com>
2025-03-20 12:14:12 +0900

    feat :: 공고 조회

---
+++ src/main/java/com/example/daedongv3_5/domain/announcement/domain/application/QueryAnnouncementService.java
@@ -0,0 +1,33 @@
+package com.example.daedongv3_5.domain.announcement.domain.application;
+
+import com.example.daedongv3_5.domain.announcement.domain.Announcement;
+import com.example.daedongv3_5.domain.announcement.domain.repository.AnnouncementRepository;
+import com.example.daedongv3_5.domain.announcement.exception.AnnouncementNotFoundException;
+import com.example.daedongv3_5.domain.announcement.presentation.dto.response.AnnouncementResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class QueryAnnouncementService {
+    private final AnnouncementRepository announcementRepository;
+
+    @Transactional(readOnly = true)
+    public AnnouncementResponse queryAnnouncement(Long id) {
+
+        Announcement announcement = announcementRepository.findById(id)
+                .orElseThrow(() -> AnnouncementNotFoundException.EXCEPTION);
+
+        return AnnouncementResponse.builder()
+                .introduction(announcement.getIntroduction())
+                .clubName(announcement.getClub().getClubName())
+                .idealTalent(announcement.getIdealTalent())
+                .interviewProject(announcement.getInterviewProject())
+                .majors(new ArrayList<>(announcement.getMajors()))
+                .announcementStatus(announcement.getAnnouncementStatus())
+                .build();
+    }
+}
