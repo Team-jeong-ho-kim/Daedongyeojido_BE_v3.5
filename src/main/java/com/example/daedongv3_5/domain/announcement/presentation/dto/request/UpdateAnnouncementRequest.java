1a7d7567b46875899502063df3d36582932f757f
troy96475 <taesupark0504@gmail.com>
2025-03-20 12:15:14 +0900

    feat :: 공고 수정

---
+++ src/main/java/com/example/daedongv3_5/domain/announcement/presentation/dto/request/UpdateAnnouncementRequest.java
@@ -0,0 +1,28 @@
+package com.example.daedongv3_5.domain.announcement.presentation.dto.request;
+
+import com.example.daedongv3_5.domain.club.domain.enums.Major;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class UpdateAnnouncementRequest {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    @Size(max = 300)
+    private String introduction;
+
+    @NotNull
+    private List<Major> majors;
+
+    @Size(max = 300)
+    private String idealTalent;
+
+    @Size(max = 100)
+    private String interviewProject;
+}
