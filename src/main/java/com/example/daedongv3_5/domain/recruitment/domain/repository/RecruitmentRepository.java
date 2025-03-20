f2ad2b2789c4f31376411983502e37ad6abb882b
troy96475 <taesupark0504@gmail.com>
2025-03-20 23:37:30 +0900

    feat :: 조회 쿼리

--- src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
@@ -1,6 +1,7 @@
 package com.example.daedongv3_5.domain.recruitment.domain.repository;
 
 import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
+import com.example.daedongv3_5.domain.recruitment.presentation.dto.response.RecruitmentListResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.Optional;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
     List<Recruitment> findRecruitmentByClub_ClubName(String clubName);
+
+    List<Recruitment> findByCreatedBy(String createdBy);
 
     Optional<Recruitment> findRecruitmentByCreatedBy(String createdBy);
 }
