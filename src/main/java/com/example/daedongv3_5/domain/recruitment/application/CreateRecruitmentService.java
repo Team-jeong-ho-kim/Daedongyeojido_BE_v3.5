b4beead992ebb68a7139450b811aa234f9727469
troy96475 <taesupark0504@gmail.com>
2025-03-20 12:32:07 +0900

    refactor :: 생성 불가 예외 적용

--- src/main/java/com/example/daedongv3_5/domain/recruitment/application/CreateRecruitmentService.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/application/CreateRecruitmentService.java
@@ -1,5 +1,9 @@
 package com.example.daedongv3_5.domain.recruitment.application;
 
+import com.example.daedongv3_5.domain.announcement.domain.Announcement;
+import com.example.daedongv3_5.domain.announcement.domain.enums.AnnouncementStatus;
+import com.example.daedongv3_5.domain.announcement.domain.repository.AnnouncementRepository;
+import com.example.daedongv3_5.domain.announcement.exception.AnnouncementNotFoundException;
 import com.example.daedongv3_5.domain.auth.service.facade.UserFacade;
 import com.example.daedongv3_5.domain.club.domain.Club;
 import com.example.daedongv3_5.domain.club.domain.repository.ClubRepository;
@@ -7,6 +11,7 @@ import com.example.daedongv3_5.domain.club.exception.ClubNotFoundException;
 import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
 import com.example.daedongv3_5.domain.recruitment.domain.enums.RecruitmentStatus;
 import com.example.daedongv3_5.domain.recruitment.domain.repository.RecruitmentRepository;
+import com.example.daedongv3_5.domain.recruitment.exception.CannotCreateRecruitmentException;
 import com.example.daedongv3_5.domain.recruitment.presentation.dto.request.RecruitmentRequest;
 import com.example.daedongv3_5.domain.student.domain.StudentEntity;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreateRecruitmentService {
 
     private final RecruitmentRepository recruitmentRepository;
+    private final AnnouncementRepository announcementRepository;
     private final ClubRepository clubRepository;
     private final UserFacade userFacade;
 
@@ -27,6 +33,13 @@ public class CreateRecruitmentService {
                         .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
 
         StudentEntity student = userFacade.currentUser();
+
+        Announcement announcement = announcementRepository.findAnnouncementByClubName(request.getClubName())
+                .orElseThrow(() -> AnnouncementNotFoundException.EXCEPTION);
+
+        if (announcement.getAnnouncementStatus() != AnnouncementStatus.SubmissionPossible) {
+            throw CannotCreateRecruitmentException.EXCEPTION;
+        }
 
         recruitmentRepository.save(Recruitment.builder()
                 .introduction(request.getIntroduction())
