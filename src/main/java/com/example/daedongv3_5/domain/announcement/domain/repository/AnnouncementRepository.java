d8e38f77bebe12b7cf6de1f5f93da59924e9247c
troy96475 <taesupark0504@gmail.com>
2025-03-20 19:44:49 +0900

    refactor :: query 네이밍 변경

--- src/main/java/com/example/daedongv3_5/domain/announcement/domain/repository/AnnouncementRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/announcement/domain/repository/AnnouncementRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-    Optional<Announcement> findAnnouncementByClubName(String clubName);
+    Optional<Announcement> findAnnouncementByClub_ClubName(String clubName);
 }
--- src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
+++ src/main/java/com/example/daedongv3_5/domain/recruitment/domain/repository/RecruitmentRepository.java
@@ -3,5 +3,8 @@ package com.example.daedongv3_5.domain.recruitment.domain.repository;
 import com.example.daedongv3_5.domain.recruitment.domain.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+    List<Recruitment> findRecruitmentByClub_ClubName(String clubName);
 }
